c84df9931c497de5a0c6eee91cb35d3708bd1cdd
himmat <himmatrai20@gmail.com>
2024-10-06 18:36:54 +0100

    duplicate numbers in array - solution added

---
+++ data-structures/array/duplicates_in_array.js
@@ -0,0 +1,28 @@
+const arr = [4, 3, 2, 7, 8, 2, 3, 1];
+const arr0 = [4, 3, 2, 7, 8, 2, 3, 2, 1];
+const arr1 = [1];
+const arr2 = [4, 7, 8, 2, 3, 1];
+
+function duplicates(arr) {
+  const visited = new Set();
+  const duplicateSet = new Set();
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+
+    // if the element has already been seen, add it to duplicate set
+    if (visited.has(num)) {
+      duplicateSet.add(num);
+    }
+    // Mark the current number as seen
+    visited.add(num);
+  }
+
+  return Array.from(duplicateSet);
+}
+
+console.log(duplicates(arr)); // when the array contains more than one repeting elements
+console.log(duplicates(arr0)); // when the array contains more than two repeting elements
+console.log(duplicates(arr1)); // when the array has one element
+console.log(duplicates(arr2)); // when the array does not have any repeting elements
+console.log(duplicates([])); // when the array is empty
---
+++ playground.js
@@ -0,0 +1,6 @@
+/**
+ * this a file to test out anything
+ */
+
+console.log("Hello world!!");
+console.log(10 % 7);
