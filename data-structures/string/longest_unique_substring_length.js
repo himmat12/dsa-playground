3c259921f857157191202555e46a07f626716233
himmat <himmatrai20@gmail.com>
2024-10-06 23:43:27 +0100

    typo fixed in comments

---
+++ data-structures/string/longest_unique_substring_length.js
@@ -0,0 +1,8 @@
+const str0 = "abcabcbb";
+const str1 = "bbb";
+
+function maxUniqueSubstringLength(str) {
+  return "";
+}
+
+console.log(maxUniqueSubstringLength(str0));
--- data-structures/string/valid_palindrome.js
+++ data-structures/string/valid_palindrome.js
@@ -4,7 +4,7 @@ function isPalindrome(str) {
     // reversedStr stores filters and stores only lowercase chars from 'a' to 'z' as per the problem constraint
   const filteredStr = str
     .toLowerCase() // converts the string into lowercase
-    .split("") // splits each chars in a string ans return a new array of chars
+    .split("") // splits each chars in a string and return a new array of chars
     .filter((e) => e.charCodeAt(0) >= 97 && e.charCodeAt(0) <= 122) // filters only chars from 'a' to 'z'
     .join(""); // join joins all the chars in an array and returns a joined string of chars
 
